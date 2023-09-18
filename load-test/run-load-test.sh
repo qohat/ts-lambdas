e1e850d6776c20dda037710bb10ed0e9073a2e7a
Qohat Pretel Polo <qohatpp@gmail.com>
2023-09-18 18:32:45 -0500

    Fix

--- load-test/run-load-test.sh
+++ load-test/run-load-test.sh
@@ -1,7 +1,11 @@
 ##Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 ##SPDX-License-Identifier: MIT-0
 
+<<<<<<< HEAD
 STACK_NAME=AnsweringLambdasStack
+=======
+STACK_NAME=ServerlessTypescriptDemoStack
+>>>>>>> adb801b (initial)
 
 API_URL=$(aws cloudformation describe-stacks --stack-name $STACK_NAME \
   --query 'Stacks[0].Outputs[?OutputKey==`ApiURL`].OutputValue' \
@@ -9,6 +13,10 @@ API_URL=$(aws cloudformation describe-stacks --stack-name $STACK_NAME \
 
 echo $API_URL
 
+<<<<<<< HEAD
 ROOT_FOLDER=$(pwd)
 
 artillery run "$ROOT_FOLDER/load-test/load-test.yml" --target "$API_URL"
+=======
+artillery run load-test.yml --target "$API_URL"
+>>>>>>> adb801b (initial)
