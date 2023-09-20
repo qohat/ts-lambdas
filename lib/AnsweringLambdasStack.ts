1499e377d28c79118b23e997db60784b80935ba5
Qohat Pretel Polo <qohatpp@gmail.com>
2023-09-20 09:35:14 -0500

    Adds role to use SES

--- lib/AnsweringLambdasStack.ts
+++ lib/AnsweringLambdasStack.ts
@@ -6,6 +6,7 @@ import { DynamoStack } from "./DynamoStack";
 import { SqsStack } from "./SqsStack";
 import { LambdaStack } from "./LambdaStack";
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class AnsweringLambdasStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -32,6 +33,23 @@ export class AnsweringLambdasStack extends Stack {
       lambdaName: "EmailNotificationLambda",
       lambdaPath: "email-notification"
     })
+
+    // Create an IAM role for the Lambda function
+    const lambdaRole = new iam.Role(this, 'LambdaRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    });
+
+    // Attach the SES send-email permission policy to the Lambda role
+    const sesPolicy = new iam.PolicyStatement({
+      actions: ['ses:SendEmail'],
+      effect: iam.Effect.ALLOW,
+      resources: ['*'], // You can limit this to specific SES resources if needed
+    });
+
+    lambdaRole.addToPolicy(sesPolicy);
+
+    // Attach the role to the Lambda function
+    emailLambdaStack.lambda.addToRolePolicy(sesPolicy);
 
     dynamoDbStack.table.grantStreamRead(remindersLambdaStack.lambda);
     queueStack.queue.grantSendMessages(remindersLambdaStack.lambda);
